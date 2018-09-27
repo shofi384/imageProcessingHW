bacba6cddf55f9ddfab4bad6b65519667d2f2386
Bashir Ahamed <ahamedb1991@gmail.com>
2018-09-26 22:53:53 -0400

    fixed total initial assignment =

--- hw1/HW_quantize.cpp
+++ hw1/HW_quantize.cpp
@@ -9,7 +9,7 @@ HW_quantize(ImagePtr I1, int levels, bool dither, ImagePtr I2)
 {
 	int w = I1->width();
 	int h = I1->height();
-	int total - w * h;
+	int total =  w * h;
 
 	int i, lut[MXGRAY];
 
@@ -30,7 +30,7 @@ HW_quantize(ImagePtr I1, int levels, bool dither, ImagePtr I2)
 	if( !dither) {
 		int type;
 		ChannelPtr<uchar> p1, p2, endPtr;
-		for(int char = 0; IP_getChannel(I1, ch, p1, type); ++ch) {
+		for(int ch = 0; IP_getChannel(I1, ch, p1, type); ++ch) {
 			IP_getChannel(I2, ch, p2, type);
 			for(endPtr = p1 + total; endPtr > p1; )
 				*p2++ = lut[*p1++];
