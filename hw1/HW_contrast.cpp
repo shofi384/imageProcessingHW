2710fcb120e07a28057f53a7e454d5a89d234a7b
uchajk <ucha@localhost.localdomain>
2018-09-26 23:31:36 -0400

    contrast stretching is working now

--- hw1/HW_contrast.cpp
+++ hw1/HW_contrast.cpp
@@ -9,4 +9,20 @@
 void
 HW_contrast(ImagePtr I1, double brightness, double contrast, ImagePtr I2)
 {
+  IP_copyImageHeader(I1, I2);
+
+  int w = I1->width();
+  int h = I1->height();
+  int total = w * h;
+
+  int i, lut[MXGRAY];
+  for(i=0; i<MXGRAY; i++) lut[i] = std::min(std::max((int)std::round((i - 128)*contrast + 128 + brightness),0),255);
+
+  ChannelPtr<uchar> p1, p2;
+  int type;
+
+  for(int ch=0; IP_getChannel(I1, ch, p1, type); ch++)
+    IP_getChannel(I2, ch, p2, type);
+
+  for(i=0; i<total; i++) *p2++ = lut[*p1++];
 }
