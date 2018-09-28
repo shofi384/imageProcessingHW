757e785d8878dbc5d5613c9d797ff5b4a611ab12
uchajk <ucha@localhost.localdomain>
2018-09-28 14:45:26 -0400

    quantize equal intervals

--- hw1/HW_quantize.cpp
+++ hw1/HW_quantize.cpp
@@ -13,38 +13,36 @@
 void
 HW_quantize(ImagePtr I1, int levels, bool dither, ImagePtr I2)
 {
-	int w = I1->width();
-	int h = I1->height();
-	int total =  w * h;
+  int w = I1->width();
+  int h = I1->height();
+  int total =  w * h;
 
-	int i, lut[MXGRAY];
+  int i, lut[MXGRAY];
 
-	IP_copyImageHeader(I1, I2);
+  IP_copyImageHeader(I1, I2);
 
-	double scale = double(MXGRAY)/levels;
+  double scale =(double)MXGRAY/levels;
+  double step = (double)MXGRAY/(levels-1);
 
-	// assign the values of the look up table
+  for(i = 0; i < MXGRAY; ++i)
+    lut[i] = CLIP((int)(floor(i/scale)*step),0,255);
 
-	for(i = 0; i < MXGRAY; ++i)
-		 // Use maro function CLIP(int a, int b, int c): min(max(a.b),c)
-		lut[i] = CLIP(int( floor(i/scale) * scale + scale/2 ), 0, MXGRAY);
-
-	// dither disabled
+  // dither disabled
 	
-	if( !dither) {
-		int type;
-		ChannelPtr<uchar> p1, p2, endPtr;
-		for(int ch = 0; IP_getChannel(I1, ch, p1, type); ++ch) {
-			IP_getChannel(I2, ch, p2, type);
-			for(endPtr = p1 + total; endPtr > p1; )
-				*p2++ = lut[*p1++];
-		}
-	}
+  if( !dither) {
+    int type;
+    ChannelPtr<uchar> p1, p2, endPtr;
+    for(int ch = 0; IP_getChannel(I1, ch, p1, type); ++ch) {
+      IP_getChannel(I2, ch, p2, type);
+      for(endPtr = p1 + total; endPtr > p1; )
+	*p2++ = lut[*p1++];
+    }
+  }
 
 
-	// dither is enabled
+  // dither is enabled
 	
-	else {
+  else {
 
-	}
+  }
 }
